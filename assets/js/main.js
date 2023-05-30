9565572920cc965b40194cc33668cd3a4db4666e
Roman Parfentev <romanntev@gmail.com>
2023-05-30 08:37:38 +0200

    edit function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ const apiKey = "3ce489c1864798a092ec39d1700a891f";
 //! function for coordinates
 document.querySelector("button").addEventListener("click", () => {
   const input = document.querySelector(".input").value;
+  const blockToHide = document.querySelector(".change-hide");
+  const hideToBlock = document.querySelector(".change-block");
   let coordinates = [];
 
   //! check if city or zip code
@@ -11,7 +13,7 @@ document.querySelector("button").addEventListener("click", () => {
   if (isNaN(input)) {
     const city = input;
     fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -25,7 +27,7 @@ document.querySelector("button").addEventListener("click", () => {
   } else {
     const zipCode = Number(input);
     fetch(
-      `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},DE&appid=${apiKey}`
+      `https://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},DE&appid=${apiKey}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -44,6 +46,7 @@ document.querySelector("button").addEventListener("click", () => {
       .then((res) => res.json())
       .then((data) => {
         //! data
+        const city = data.name;
         const temp = Math.round(data.main.temp - 273.15);
         const cloud = data.clouds.all;
         const wind = Math.round(data.wind.speed * 3.6);
@@ -51,36 +54,15 @@ document.querySelector("button").addEventListener("click", () => {
         const humidity = data.main.humidity;
         const pressure = data.main.pressure;
         const icon = data.weather[0].icon;
-
-        //! Milliseconds in time for sunrise
-        function sunriseInTime(milliseconds) {
-          let datum = new Date(milliseconds);
-          let stunden = datum.getHours();
-          let minuten = datum.getMinutes();
-          let sekunden = datum.getSeconds();
-          stunden = stunden < 10 ? "0" + stunden : stunden;
-          minuten = minuten < 10 ? "0" + minuten : minuten;
-          sekunden = sekunden < 10 ? "0" + sekunden : sekunden;
-          let uhrzeit = stunden + ":" + minuten + ":" + sekunden;
-          return uhrzeit;
-        }
-        const sunrise = data.sys.sunrise;
-        let uhrzeitSunrise = sunriseInTime(sunrise * 1000);
-
-        //! Milliseconds in time for sunset
-        function sunsetInTime(milliseconds) {
-          let datum = new Date(milliseconds);
-          let stunden = datum.getHours();
-          let minuten = datum.getMinutes();
-          let sekunden = datum.getSeconds();
-          stunden = stunden < 10 ? "0" + stunden : stunden;
-          minuten = minuten < 10 ? "0" + minuten : minuten;
-          sekunden = sekunden < 10 ? "0" + sekunden : sekunden;
-          let uhrzeit = stunden + ":" + minuten + ":" + sekunden;
-          return uhrzeit;
-        }
-        const sunset = data.sys.sunset;
-        let uhrzeitSunset = sunsetInTime(sunset * 1000);
+        const sunRise = new Date(data.sys.sunrise * 1000).toLocaleTimeString(
+          "de-DE"
+        );
+        const sunSet = new Date(data.sys.sunset * 1000).toLocaleTimeString(
+          "de-DE"
+        );
+        let localeTime = new Date().getTime();
+        let date = new Date(localeTime - 3600 * 2000 + data.timezone * 1000);
+        timeZone = date.toLocaleTimeString();
 
         //! import Icon & Temperature in HTML
         let image = document.createElement("img");
@@ -96,6 +78,10 @@ document.querySelector("button").addEventListener("click", () => {
 
         //! import remaining data in HTML
         let content = `<div>
+                        <h3>${timeZone} </h3>
+                        <p>Lokale Zeit</p>
+                        </div>
+                        <div>
                         <h3>${wind} km/h</h3>
                         <p>Windgeschwindigkeit</p>
                         </div>
@@ -104,22 +90,21 @@ document.querySelector("button").addEventListener("click", () => {
                         <p>Feutchtigkeit</p>
                         </div>
                         <div>
-                        <h3>${pressure} hPa</h3>
-                        <p>Atmosphärischer Druck</p>
-                        </div>
-                        <div>
                         <h3>${cloud} %</h3>
                         <p>Bewölkt</p>
                         </div>
                         <div>
-                        <h3>${uhrzeitSunrise} </h3>
+                        <h3>${sunRise} </h3>
                         <p>Sonnenaufgang</p>
                         </div>
                         <div>
-                        <h3>${uhrzeitSunset} </h3>
+                        <h3>${sunSet} </h3>
                         <p>Sonnenuntergang</p>
                         </div>`;
         document.querySelector(".data").innerHTML = content;
+        blockToHide.style.display = "none";
+        hideToBlock.style.display = "block";
+        hideToBlock.innerHTML = `<h1>${city}</h1> <h2>${timeZone}</h2>`;
       })
       .catch((error) => {
         console.log("Fehler: ", error);
